b7062c62e478abd91c49a4134106777ea41b4ee9
Sweetysinghcdac <sweetysinghcdac@gmail.com>
2024-06-28 13:29:57 +0530

    initial code

---
+++ app/Models/Conversation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'reciever_id',
+        'sender_id'
+
+    ];
+    public function message(){
+        return this->hasMany(Message::class);
+    }
+}
---
+++ resources/views/livewire/users.blade.php
@@ -0,0 +1,10 @@
+<div class="max-w-6xl mx-auto my-16">
+    {{-- In work, do what you enjoy. --}}
+
+    <div class="h5 text-center text-5xl font-bold py-3">Users</div>
+
+    <div class="grid grid-cols-2 lg:grid-grid-cols-3 xl:grid-grid-cols-4 gap-5 p-2">
+
+         .w-full.bg-white.
+    </div>
+</div>
